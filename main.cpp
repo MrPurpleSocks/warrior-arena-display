00cb8cdc2ee2224a9e9543d822e5558efbfee288
Patrick Woollvin <patrick@warriorb.org>
2024-11-06 22:15:30 -0800

    IT ACTUALLY WORKS

--- main.cpp
+++ main.cpp
@@ -1,19 +1,67 @@
-#include "pico_explorer.hpp"
-#include "drivers/st7789/st7789.hpp"
 #include "libraries/pico_graphics/pico_graphics.hpp"
+#include "libraries/interstate75/interstate75.hpp"
+#include "rgbled.hpp"
+#include "button.hpp"
+#include "pico/stdlib.h"
 
 using namespace pimoroni;
 
-ST7789 st7789(PicoExplorer::WIDTH, PicoExplorer::HEIGHT, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
-PicoGraphics_PenRGB332 graphics(st7789.width, st7789.height, nullptr);
+// Display driver for a single 32x32 hub75 matrix
+Hub75 hub75(64, 32, nullptr, PANEL_GENERIC, false);
+
+// Graphics library - in 24Bit mode with 16M colours
+PicoGraphics_PenRGB888 graphics(hub75.width, hub75.height, nullptr);
+
+// And each button
+Button button_a(Interstate75::A);
+// For the Interstate75
+// Button button_b(Interstate75::BOOT); // Using this button definition on the Interstate75W will most likely disable the wifi make sure it is commented out if using Interstate75W
+
+// Or for the Interstate75W
+Button button_b(Interstate75::B); // This button is not present on the Interstate75 (non W version)
+// RGB LED
+RGBLED led(Interstate75::LED_R, Interstate75::LED_G, Interstate75::LED_B, ACTIVE_LOW);
+
+// Interrupt callback required function 
+void __isr dma_complete() {
+  hub75.dma_complete();
+}
 
 int main() {
-    graphics.set_pen(255, 0, 0);
+    hub75.start(dma_complete);
+    led.set_rgb(0,0,0);
+    graphics.set_pen(0, 0, 0);
+    graphics.clear();
+
+    Rect amberBorder(hub75.width-16, hub75.height-16, hub75.width-2, hub75.height-2); // set amber border
+    graphics.set_pen(255, 165, 0);
+    graphics.rectangle(amberBorder);
+    hub75.update(&graphics);
+
+    //Rect allianceBorder(hub75.width-32, hub75.height-16, hub75.width-19, hub75.height-2); // set alliance border
+    //graphics.set_pen(1, 79, 143);
+    //graphics.rectangle(allianceBorder);
+    //hub75.update(&graphics);
+
+    Rect amberFill(hub75.width-15, hub75.height-15, hub75.width-3, hub75.height-3); // set amber fill
+    Rect allianceFill(hub75.width-31, hub75.height-15, hub75.width-19, hub75.height-3); // set alliance fill
 
     while(true) {
-        graphics.pixel(Point(0, 0));
+        graphics.set_pen(255, 165, 0);
+        graphics.rectangle(amberFill); // fill amber
+        hub75.update(&graphics);
+        //graphics.set_pen(1, 79, 143);
+        //graphics.rectangle(allianceFill); // fill alliance
+        //hub75.update(&graphics);
 
-        // now we've done our drawing let's update the screen
-        st7789.update(&graphics);
+        sleep_ms(1000);
+
+        graphics.set_pen(0, 0, 0);
+        graphics.rectangle(amberFill); // clear amber
+        hub75.update(&graphics);
+        //graphics.rectangle(allianceFill); // fill alliance
+        //hub75.update(&graphics);
+
+        sleep_ms(1000);
     }
 }
